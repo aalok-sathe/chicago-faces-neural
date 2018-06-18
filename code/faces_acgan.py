35665ff5eb441d4230e101dc332bb75775bd49d3
aalok.sathe <aalok.sathe@gmail.com>
2018-06-18 14:03:41 -0400

    renamed:    faces-acgan.py -> faces_acgan.py 	renamed:    get-face.py -> get_face.py

--- code/faces_acgan.py
+++ code/faces_acgan.py
@@ -8,7 +8,7 @@ from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
-import
+import get_face
 
 import matplotlib.pyplot as plt
 
