b58b4da400a88f759f89d38249b62b1bd726125f
aalok.sathe <aalok.sathe@gmail.com>
2018-06-19 15:14:55 -0400

    fix syntaxerror in try-except block (missing `except') 	modified:   faces_acgan.py

--- code/faces_acgan.py
+++ code/faces_acgan.py
@@ -186,6 +186,10 @@ class ACGAN():
                 with open("runtime.kerasconfig", 'r') as file:
                     runtime_params = json.load(file)
                     sample_interval = runtime_params.get("sample_every", sample_interval)
+            except IOError as e:
+                print(e)
+            except FileNotFoundError as e:
+                print(e)
             if epoch % sample_interval == 0:
                 self.save_model()
                 self.sample_images(epoch)
