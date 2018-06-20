c31be55e817ab9de6395227988878bbcd42329d6
aalok.sathe <aalok.sathe@gmail.com>
2018-06-20 08:37:15 -0400

    correct cvtColor API

--- code/get_face.py
+++ code/get_face.py
@@ -138,7 +138,7 @@ class face_provider:
         # img = pickle.load(open(self.images[rac][gen][emo][id]), 'rb') # WIP
         img = self.resize(rac, gen, emo, id, resize=resize)
         if grayscale:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.cvtColor(img, img, cv2.COLOR_BGR2GRAY)
         return self.images[rac][gen][emo][id]
 
     def list_faces(self, rac=None, gen=None, emo=None):
